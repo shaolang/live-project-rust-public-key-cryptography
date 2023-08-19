3161391663f8b0821b81defe23bf743ebf1ad268
shaolang <shaolang@ymail.com>
2023-08-19 19:25:42 +0800

    Complete main program for milestone 6

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,35 @@
 use std::io::{self, Write};
-use std::time::Instant;
 use rpkc;
 
 const NUM_TESTS: i64 = 20;
 
 fn main() {
     let mut rng = rpkc::Prng::new();
+    let p = rpkc::find_prime(&mut rng, 1_000, 10_000, NUM_TESTS as u32) as i64;
+    let q = rpkc::find_prime(&mut rng, 1_000, 10_000, NUM_TESTS as u32) as i64;
+    let n = p * q;
+    let ln = rpkc::totient(p, q);
+    let e = rpkc::random_exponent(&mut rng, ln);
+    let d = rpkc::inverse_mod(e, ln);
 
-    let probability = 100.0 - 100.0 / rpkc::fast_exp(2, NUM_TESTS) as f64;
-    println!("Probability: {}%\n", probability);
+    println!("*** Public ***");
+    println!("   Public key modulus: {n}");
+    println!("Public key exponent e: {e}");
+    println!("\n*** Private ***");
+    println!("Primes: {p}, {q}");
+    println!("  λ(n): {ln}");
+    println!("     d: {d}");
 
     loop {
-        let num_digits = get_i64("# Digits (max 9): ");
-        if num_digits < 1 { break; }
+        let message = get_i64("\n   Message: ");
 
-        let mut min = 10i64.pow((num_digits - 1) as u32);
-        let max = 10 * min;
+        if message < 2 || message > n - 1 { break; }
 
-        if min == 1 { min = 2; }
+        let encrypted = rpkc::fast_exp_mod(message, e, n);
+        let decrypted = rpkc::fast_exp_mod(encrypted, d, n);
 
-        println!("Prime: {}", rpkc::find_prime(&mut rng, min, max, NUM_TESTS as u32));
+        println!("Ciphertext: {encrypted}");
+        println!(" Plaintext: {decrypted}");
     }
 }
 
@@ -36,27 +46,4 @@ fn get_i64(prompt: &str) -> i64 {
     let trimmed = str_value.trim();
 
     trimmed.parse::<i64>().expect("Error parsing integer")
-}
-
-
-fn simple_benchmark(primes: &[i64]) {
-    // find the factors the slow way
-    let start1 = Instant::now();
-    let num = 44711100255155897;
-    let mut factors1 = rpkc::find_factors(num);
-    let duration1 = start1.elapsed();
-
-    println!("find_factors: {duration1:?} seconds");
-    rpkc::print_numbers(&mut factors1);
-    println!("Product: {}", rpkc::multiply_vector(&factors1));
-
-    // use Euler's sieve to find the factors
-    let start2 = Instant::now();
-    let num = 312680865509917;
-    let mut factors2 = rpkc::find_factors_sieve(&primes, num);
-    let duration2 = start2.elapsed();
-
-    println!("find_factors: {duration2:?} seconds");
-    rpkc::print_numbers(&mut factors2);
-    println!("Product: {}", rpkc::multiply_vector(&factors2));
 }
